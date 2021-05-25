9f954a745a477a7aa9c5e66a29339b75f1ed174d
Pooja Bhore <bhorepooja14@gmail.com>
2021-05-25 08:29:45 +0530

    Update style.php

--- style.php
+++ style.php
@@ -61,7 +61,7 @@
           margin: 0 0 30px 0;
     }
 
-    .corona-update h2{
+    .corona-update h1{
           color: #ff7675;
     }
 
@@ -119,7 +119,7 @@
               width: 150px;
               text-align: center;
               border: 1px solid black;
-              padding: 5px;
+              padding: 7px;
          }
     .mytbl{
               display: flex;
@@ -169,15 +169,73 @@
               opacity: 1;
               visibility: visible;
     }
+
+
+    .card {
+              background-color: #fff;
+              border-radius: 10px;
+              border: none;
+              position: relative;
+              margin-bottom: 30px;
+              box-shadow: 0 0.46875rem 2.1875rem rgba(90,97,105,0.1), 0 0.9375rem 1.40625rem rgba(90,97,105,0.1), 0 0.25rem 0.53125rem rgba(90,97,105,0.12), 0 0.125rem 0.1875rem rgba(90,97,105,0.1);
+     }
+
+     .newconfirm {
+             background:  linear-gradient(to right top, #e0d828, #e7e124, #efea1f, #f6f418, #fdfd0e)!important;
+             color: #fff;
+     }
+
+    .totalconfirm {
+             background: linear-gradient(to right top, #e3e315, #e6da1e, #e7d125, #e7c82c, #e7c033)  !important;
+             color: #fff;
+     }
+
+    .newdeath{
+             background:  linear-gradient(to right top, #e31515, #e52e2a, #e5403d, #e54f4d, #e35d5d) !important;
+             color: #fff;
+     }
+
+     .totaldeath {
+             background:  linear-gradient(to right top, #e31515, #e91414, #f01212, #f61010, #fd0e0e) !important;
+             color: #fff;
+     }
+
+    .newrecover {
+             background: linear-gradient(to right top, #12ee2e, #16e444, #21d954, #2ccf60, #38c469) !important;
+             color: #fff; 
+     }
+
+    .totalrecover{
+             background: linear-gradient(to right top, #3fcb51, #36ca63, #31c873, #32c681, #38c48d) !important;
+             color: #fff;
+     }
+
+     .card .card-statistic-3 .card-icon-large .fas, .card .card-statistic-3 .card-icon-large .far, .card .card-statistic-3 .card-icon-large .fab, .card .card-statistic-3 .card-icon-large .fal {
+             font-size: 110px;
+      }
+
+    .card .card-statistic-3 .card-icon {
+             text-align: center;
+             line-height: 50px;
+             margin-left: 15px;
+             color: #000;
+             position: absolute;
+             right: -5px;
+             top: 20px;
+             opacity: 0.1;
+     }
+
     .drop{
-         background-color: #a29bfe!important;
+             background-color: #a29bfe!important;
          
-
     }
+
     .drop a{
-     background-color: #a29bfe!important;
-
+             background-color: #a29bfe!important;
     }
+
+
+
 
 
 </style>
